648d6ca325452b96fb0cbe206a7ffe86dd46f7bb
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-08-23 16:16:04 +0000

    Deploy EricLambrecht/better-spotify-playlists to github.com/EricLambrecht/better-spotify-playlists.git:gh-pages

--- src/components/SpotifyPlaylistSelector.vue
+++ src/components/SpotifyPlaylistSelector.vue
@@ -7,60 +7,55 @@
 </template>
 
 <script>
-import SpotifyWebApi from 'spotify-web-api-js';
+  import SpotifyWebApi from 'spotify-web-api-js';
 
-import 'moment-duration-format';
+  import 'moment-duration-format';
 
-let s = new SpotifyWebApi();
-s.setAccessToken('');
+  let s = new SpotifyWebApi();
+  s.setAccessToken('');
 
-export default {
-  name: 'spotify-playlist-selector',
-  data () {
-    return {
-      playlistURI: "spotify:user:1127316932:playlist:0pLfNXXyU21MWIv0tP3hwH",
-    }
-  },
-  computed: {
-    playlistURIData() {
-      // example: spotify:user:1127316932:playlist:0pLfNXXyU21MWIv0tP3hwH
-      const search = this.playlistURI.match(/.*user:([^\s]+):playlist:([^\s]+)/i);
-      if (search === null) {
-          return null;
-      }
+  export default {
+    name: 'spotify-playlist-selector',
+    data() {
       return {
-        userId: search[1],
-        id: search[2],
-      };
-    }
-  },
-  methods: {
-    getTracks(offset = 0, limit = 0) {
-      // Do we have all tracks?
-      return s.getPlaylistTracks(this.playlistURIData.id, {
-        limit: limit,
-        offset: offset
-      }).then((tracks) => {
-        console.log("getPlaylistTracks", tracks);
-        const fetchedTracks = tracks.items.length + tracks.offset;
-        if(fetchedTracks < tracks.total) {
-          this.getTracks(fetchedTracks, 100).then((remainingTracks) => {
-            tracks.items = tracks.items.concat(remainingTracks.items);
-          }).catch((err) => {
-            console.warn(err);
-          });
-        }
-        return Promise.resolve(tracks);
-      })
+        playlistURI: "spotify:user:1127316932:playlist:0pLfNXXyU21MWIv0tP3hwH",
+      }
     },
-    fetchPlaylist () {
-      console.log('this.playlistURIData', this.playlistURIData);
-      s.getPlaylist(this.playlistURIData.id)
-        .then((data) => {
+    computed: {
+      playlistId() {
+        // example: spotify:user:1127316932:playlist:0pLfNXXyU21MWIv0tP3hwH
+        const search = this.playlistURI.match(/.*user:[^\s]+:playlist:([^\s]+)/i);
+        if (search === null) {
+          return null;
+        }
+        return search[1];
+      }
+    },
+    methods: {
+      getTracks(offset = 0, limit = 0) {
+        // Do we have all tracks?
+        return s.getPlaylistTracks(this.playlistId, {
+          limit: limit,
+          offset: offset
+        }).then((tracks) => {
+          const fetchedTracks = tracks.items.length + tracks.offset;
+          if (fetchedTracks < tracks.total) {
+            this.getTracks(fetchedTracks, 100).then((remainingTracks) => {
+              tracks.items = tracks.items.concat(remainingTracks.items);
+            }).catch((err) => {
+              console.warn(err);
+            });
+          }
+          return Promise.resolve(tracks);
+        })
+      },
+      async fetchPlaylist() {
+        try {
+          const data = await s.getPlaylist(this.playlistId);
 
           // Do we have all tracks?
           const fetchedTracks = data.tracks.items.length + data.tracks.offset;
-          if(fetchedTracks < data.tracks.total) {
+          if (fetchedTracks < data.tracks.total) {
             this.getTracks(fetchedTracks, 100).then((remainingTracks) => {
               data.tracks.items = data.tracks.items.concat(remainingTracks.items);
               // Emit event for parents...
@@ -73,22 +68,22 @@ export default {
             // Emit event for parents...
             this.$emit('select', data);
           }
-        }, (err) => {
-          console.log('err', err);
+        }
+        catch (err) {
           const res = JSON.parse(err.response);
 
           // See if access token expired
-          if(res.error && res.error.status === 401) {
+          if (res.error && res.error.status === 401) {
             this.$emit('error', {message: "Token expired", tokenExpired: true});
           }
           else {
             console.warn(res);
             this.$emit('error', {message: "An unknown error occured"});
           }
-        });
+        }
+      },
     },
-  },
-}
+  }
 </script>
 
 <style lang="scss">
--- src/components/SpotifyTrackList.vue
+++ src/components/SpotifyTrackList.vue
@@ -1,41 +1,43 @@
 <template>
   <ol class="track-list">
     <template v-for="(item, index) in trackItems">
-       <li class="track">
-         <h4 v-if="index === 0 || item.track.first_of_hour">{{item.track.relative_start_time_ms | formatTime('h:mm')}} Uhr</h4>
-         <p>{{item.track.relative_start_time_ms  | formatTime('h:mm')}} - {{item.track.artists[0].name}} - {{ item.track.name }} ({{item.track.duration_ms | formatTime('mm:ss')}})</p>
-       </li>
+      <li class="track">
+        <h4 v-if="index === 0 || item.track.first_of_hour">{{item.track.relative_start_time_ms |
+          formatTime('h:mm')}} Uhr</h4>
+        <p>{{item.track.relative_start_time_ms | formatTime('h:mm')}} - {{item.track.artists[0].name}} - {{
+          item.track.name }} ({{item.track.duration_ms | formatTime('mm:ss')}})</p>
+      </li>
     </template>
   </ol>
 </template>
 
 <script>
-import moment from 'moment';
-import 'moment-duration-format';
+  import moment from 'moment';
+  import 'moment-duration-format';
 
-export default {
-  name: 'spotify-track-list',
-  props: ['trackItems'],
-  filters: {
-    formatTime (milliseconds, format = "") {
-      const duration = moment.duration(milliseconds);
+  export default {
+    name: 'spotify-track-list',
+    props: ['trackItems'],
+    filters: {
+      formatTime(milliseconds, format = "") {
+        const duration = moment.duration(milliseconds);
 
-      if(format) {
-        return duration.format(format);
-      }
+        if (format) {
+          return duration.format(format);
+        }
 
-      if(duration.days() > 0) {
-        return duration.format('D h:mm');
+        if (duration.days() > 0) {
+          return duration.format('D h:mm');
+        }
+        else if (duration.hours() > 0) {
+          return duration.format('h:mm');
+        }
+        else {
+          return duration.format('mm:ss');
+        }
       }
-      else if(duration.hours() > 0) {
-        return duration.format('h:mm');
-      }
-      else {
-        return duration.format('mm:ss');
-      }
-    }
-  },
-}
+    },
+  }
 </script>
 
 <style lang="scss">

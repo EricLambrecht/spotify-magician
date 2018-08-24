2efdc42cc07123b427530786bc5f081268e19ce0
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-08-24 15:15:55 +0000

    Deploy EricLambrecht/better-spotify-playlists to github.com/EricLambrecht/better-spotify-playlists.git:gh-pages

--- src/components/SpotifyTrackList.vue
+++ src/components/SpotifyTrackList.vue
@@ -6,45 +6,47 @@
         class="track">
         <h4 v-if="index === 0 || item.track.first_of_hour">{{ item.track.relative_start_time_ms |
         formatTime('h:mm') }} Uhr</h4>
-        <p>{{ item.track.relative_start_time_ms | formatTime('h:mm') }} - {{ item.track.artists[0].name }} - {{
-        item.track.name }} ({{ item.track.duration_ms | formatTime('mm:ss') }})</p>
+        <p>
+          {{ item.track.relative_start_time_ms | formatTime('h:mm') }} -
+          {{ item.track.artists[0].name }} -
+          {{ item.track.name }} ({{ item.track.duration_ms | formatTime('mm:ss') }})
+        </p>
       </li>
     </template>
   </ol>
 </template>
 
 <script>
-  import moment from 'moment';
-  import 'moment-duration-format';
+import moment from 'moment';
+import 'moment-duration-format';
 
-  export default {
-    name: 'SpotifyTrackList',
-    filters: {
-      formatTime(milliseconds, format = "") {
-        const duration = moment.duration(milliseconds);
+export default {
+  name: 'SpotifyTrackList',
+  filters: {
+    formatTime(milliseconds, format = '') {
+      const duration = moment.duration(milliseconds);
 
-        if (format) {
-          return duration.format(format);
-        }
-
-        if (duration.days() > 0) {
-          return duration.format('D h:mm');
-        }
-        else if (duration.hours() > 0) {
-          return duration.format('h:mm');
-        }
-        else {
-          return duration.format('mm:ss');
-        }
+      if (format) {
+        return duration.format(format);
       }
-    },
-    props: {
-      trackItems: {
-        type: Array,
-        required: true
+
+      if (duration.days() > 0) {
+        return duration.format('D h:mm');
       }
+      if (duration.hours() > 0) {
+        return duration.format('h:mm');
+      }
+        
+      return duration.format('mm:ss');
     },
-  }
+  },
+  props: {
+    trackItems: {
+      type: Array,
+      required: true,
+    },
+  },
+};
 </script>
 
 <style lang="scss">

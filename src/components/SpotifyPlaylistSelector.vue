837419a205efb1fdebfe4ddfb678108bb2b73b00
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-08-28 18:44:44 +0000

    Deploy EricLambrecht/better-spotify-playlists to github.com/EricLambrecht/better-spotify-playlists.git:gh-pages

--- src/components/SpotifyPlaylistSelector.vue
+++ src/components/SpotifyPlaylistSelector.vue
@@ -1,11 +1,45 @@
 <template>
   <div class="spotify-playlist-selector">
-    <label for="playlistURI">Enter Playlist URI: </label>
-    <input 
-      id="playlistURI" 
-      v-model="playlistURI" 
-      type="text">
-    <button @click="fetchPlaylist">Playlist suchen</button>
+    <div 
+      v-if="mode === 'custom'" 
+      class="form">
+      <label 
+        for="playlistURI" 
+        class="label">Enter playlist URI: </label>
+      <input
+        id="playlistURI"
+        v-model="playlistURI"
+        type="text"
+        class="input uri-input"
+      >
+      <button 
+        class="fetch-button" 
+        @click="fetchPlaylist">Playlist laden</button>
+    </div>
+    <div 
+      v-else 
+      class="form">
+      <label 
+        for="playlistId" 
+        class="label">Choose a playlist: </label>
+      <select 
+        id="playlistId" 
+        v-model="playlistId" 
+        class="input select-box" 
+        @change="fetchPlaylist">
+        <option 
+          v-for="playlist in userPlaylists"
+          :key="playlist.id"
+          :value="playlist.id">
+          {{ playlist.name }}
+        </option>
+      </select>
+    </div>
+    <button 
+      class="mode-button" 
+      @click="switchMode">
+      {{ mode === 'user' ? 'Enter manually' : 'Choose from your playlists' }}
+    </button>
   </div>
 </template>
 
@@ -20,10 +54,13 @@ export default {
   data() {
     return {
       playlistURI: 'spotify:user:1127316932:playlist:0pLfNXXyU21MWIv0tP3hwH',
+      userPlaylists: [],
+      playlistId: null,
+      mode: 'user',
     };
   },
   computed: {
-    playlistId() {
+    parsedPlaylistId() {
       // example: spotify:user:1127316932:playlist:0pLfNXXyU21MWIv0tP3hwH
       const search = this.playlistURI.match(/.*user:[^\s]+:playlist:([^\s]+)/i);
       if (search === null) {
@@ -32,11 +69,23 @@ export default {
       return search[1];
     },
   },
+  async mounted() {
+    try {
+      this.userPlaylists = await spotifyApi.getUserPlaylists();
+    } catch (err) {
+      this.$emit('error', { message: err.message });
+    }
+  },
   methods: {
     async fetchPlaylist() {
       try {
-        const data = await spotifyApi.getFullPlaylist(this.playlistId);
-        this.$emit('select', data); // emit event for parents (TODO: add state management)
+        let playlist;
+        if (this.mode === 'custom') {
+          playlist = await spotifyApi.getFullPlaylist(this.parsedPlaylistId);
+        } else {
+          playlist = await spotifyApi.getFullPlaylist(this.playlistId);
+        }
+        this.$emit('select', playlist); // emit event for parents (TODO: add state management)
       } catch (err) {
         if (err.message === 'Token expired') {
           this.$emit('error', { message: err.message, tokenExpired: true });
@@ -45,10 +94,36 @@ export default {
         }
       }
     },
+    switchMode() {
+      this.mode = this.mode === 'user' ? 'custom' : 'user';
+    },
   },
 };
 </script>
 
 <style lang="scss">
+  .spotify-playlist-selector {
+    .form {
+      display: flex;
+      flex-direction: row;
+      justify-content: center;
+      align-items: center;
 
+      .label {
+        margin-right: 10px;
+      }
+      .input {
+        padding: 5px 10px;
+      }
+      .select-box {
+      }
+      .fetch-button {
+        padding: 4px 10px 5px;
+        margin-left: 2px;
+      }
+    }
+    .mode-button {
+      margin-top: 5px;
+    }
+  }
 </style>

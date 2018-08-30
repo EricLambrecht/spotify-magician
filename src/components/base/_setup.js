01ed813b29051dc68b52be7c12e4b7e7e11dbf51
Eric Lambrecht <ericlambrecht.hh@gmail.com>
2018-08-30 20:45:41 +0200

    refactor: Added Text base component
    #1

--- src/components/base/_setup.js
+++ src/components/base/_setup.js
@@ -11,10 +11,12 @@ import Headline from './Headline.vue';
 import Link from './Link.vue';
 import NumberInput from './NumberInput.vue';
 import Paragraph from './Paragraph.vue';
+import Text from './Text.vue';
 import TextInput from './TextInput.vue';
 
 Vue.component('b-headline', Headline);
 Vue.component('b-link', Link);
 Vue.component('b-number-input', NumberInput);
 Vue.component('b-paragraph', Paragraph);
+Vue.component('b-text', Text);
 Vue.component('b-text-input', TextInput);

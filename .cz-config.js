e6eb188d721f49bc69f69f247faa97361d4c940c
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-09-01 20:19:44 +0000

    Deploy EricLambrecht/spotify-magician to github.com/EricLambrecht/spotify-magician.git:gh-pages

--- .cz-config.js
+++ .cz-config.js
@@ -1,11 +1,11 @@
 module.exports = {
 
   types: [
-    {value: 'fix',      name: 'fix:      A bug fix'},
     {value: 'feat',     name: 'feat:     A new feature'},
+    {value: 'fix',      name: 'fix:      A bug fix'},
+    {value: 'refactor', name: 'refactor: A code change that neither fixes a bug nor adds a feature'},
     {value: 'docs',     name: 'docs:     Documentation only changes'},
     {value: 'style',    name: 'style:    Markup, white-space, formatting, missing semi-colons...'},
-    {value: 'refactor', name: 'refactor: A code change that neither fixes a bug nor adds a feature'},
     {value: 'perf',     name: 'perf:     A code change that improves performance'},
     {value: 'test',     name: 'test:     Adding missing tests'},
     {value: 'chore',    name: 'chore:    Build process or auxiliary tool changes'},
@@ -30,9 +30,9 @@ module.exports = {
   // override the messages, defaults are as follows
   messages: {
     type: 'Select the type of change that you\'re committing:',
-    scope: '\nDenote the SCOPE of this change (optional):',
+    scope: '\nDenote the SCOPE of this change:',
     // used if allowCustomScopes is true
-    customScope: 'Denote the SCOPE of this change:',
+    customScope: 'Denote the SCOPE of this change (optional):',
     subject: 'Write a SHORT, IMPERATIVE tense description of the change:\n',
     body: 'Provide a LONGER description of the change (optional). Use "|" to break new line:\n',
     breaking: 'List any BREAKING CHANGES (optional):\n',

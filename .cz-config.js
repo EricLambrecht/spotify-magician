b9afe0d2b62d75cce152cdf8042af9991eef4689
Eric Lambrecht <ericlambrecht.hh@gmail.com>
2018-09-01 21:17:58 +0200

    chore(other): installed cz-customizable

---
+++ .cz-config.js
@@ -0,0 +1,49 @@
+module.exports = {
+
+  types: [
+    {value: 'fix',      name: 'fix:      A bug fix'},
+    {value: 'feat',     name: 'feat:     A new feature'},
+    {value: 'docs',     name: 'docs:     Documentation only changes'},
+    {value: 'style',    name: 'style:    Markup, white-space, formatting, missing semi-colons...'},
+    {value: 'refactor', name: 'refactor: A code change that neither fixes a bug nor adds a feature'},
+    {value: 'perf',     name: 'perf:     A code change that improves performance'},
+    {value: 'test',     name: 'test:     Adding missing tests'},
+    {value: 'chore',    name: 'chore:    Build process or auxiliary tool changes'}
+  ],
+
+  scopes: [
+    {name: 'editor'},
+    {name: 'ui/ux'},
+    {name: 'api'},
+    {name: 'other'},
+  ],
+
+  // it needs to match the value for field type. Eg.: 'fix'
+  /*
+  scopeOverrides: {
+    docs: [
+      {name: 'readme'},
+      {name: 'changelog'},
+    ]
+  },
+  */
+  // override the messages, defaults are as follows
+  messages: {
+    type: 'Select the type of change that you\'re committing:',
+    scope: '\nDenote the SCOPE of this change (optional):',
+    // used if allowCustomScopes is true
+    customScope: 'Denote the SCOPE of this change:',
+    subject: 'Write a SHORT, IMPERATIVE tense description of the change:\n',
+    body: 'Provide a LONGER description of the change (optional). Use "|" to break new line:\n',
+    breaking: 'List any BREAKING CHANGES (optional):\n',
+    footer: 'List any ISSUES CLOSED by this change (optional). E.g.: #31, #34:\n',
+    confirmCommit: 'Are you sure you want to proceed with the commit above?'
+  },
+
+  allowCustomScopes: false,
+  allowBreakingChanges: ['feat', 'fix'],
+
+  // limit subject length
+  subjectLimit: 100
+
+};

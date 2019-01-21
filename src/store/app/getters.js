b033a58bb63a2711f7fa5754375af0e4fe9922c4
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-01-21 14:28:30 +0000

    Deploy EricLambrecht/spotify-magician to github.com/EricLambrecht/spotify-magician.git:gh-pages

--- src/store/app/getters.js
+++
@@ -1,7 +0,0 @@
-export default {
-  modalIsOpen: state => modalName => state.openedModal === modalName,
-  confirmationIsPending: state => Object.keys(state.pendingConfirmation).length !== 0,
-  pendingConfirmation: state => state.pendingConfirmation,
-  toastMessages: state => state.toastMessages,
-  getToastMessage: state => id => state.toastMessages.find(toast => toast.id === id),
-};
--- src/store/user/getters.js
+++
@@ -1,3 +0,0 @@
-export default {
-  hasAccess: state => state.accessToken !== null,
-};

14a8f85997b644fde2911b0b52c97de03f9c3430
Eric Lambrecht <ericlambrecht.hh@gmail.com>
2019-01-24 00:28:26 +0100

    refactor: ♻️ Added TODO for modal refactoring

--- src/components/ModalManager.vue
+++ src/components/ModalManager.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <!-- TODO: This is not a scalable approach. We should include the modals where they are needed. -->
     <add-track-modal v-if="isOpen('add-track')" />
     <select-playlist-modal v-if="isOpen('select-playlist') || !playlistExists" />
     <changelog-modal v-if="isOpen('changelog-modal')" />
